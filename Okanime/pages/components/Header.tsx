a2aa99dcc9e1236a17f9ff5f8e0622dd2c0e5053
Burak Muhammet Kose <b.kose@se22.qmul.ac.uk>
2024-04-15 02:48:19 +0200

    header done

--- Okanime/pages/components/Header.tsx
+++ Okanime/pages/components/Header.tsx
@@ -1,22 +1,29 @@
 // components/Header.js
-
 import Image from 'next/image';
 import Link from 'next/link';
-
-import styles from '../../styles/Header.module.css';
+import "../globals.css";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <Image className="header-logo" src="/logo.png" alt="Anime Sama" width={100} height={50} />
+    <header className=" border-b border-gray-400 h-100 font-sans font-extrabold bg-custom-brown text-white">
+      <div className="flex justify-between ml-4 mr-20">
+        <div className="flex items-center">
+            <Image
+              src="/logo-okami.png"
+              alt="logo"
+              width={100}
+              height={100}
+            />
+          <h1 className="text-2xl absolute ml-20 mb-1">OKANIME</h1>
+
+        </div>
+        <div className="flex row items-center gap-x-8">
+          <Link href="/catalogue">CATALOGUE</Link>
+          <Link href="/planning">PLANNING</Link>
+          <Link href="/help">AIDE</Link>
+          <Link href="/profil">PROFIL</Link>
+        </div>
       </div>
-      <nav className={styles.nav}>
-        <Link href="/catalogue">Catalogue</Link>
-        <Link href="/planning">Planning</Link>
-        <Link href="/help">Aide</Link>
-        <Link href="/profil">Profil</Link>
-      </nav>
     </header>
   );
 };
